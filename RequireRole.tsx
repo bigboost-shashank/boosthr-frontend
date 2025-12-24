e420140c5637ab75f06a231b5b99d8f1ac4ff277
bigboost-shashank <shashank@bigboost.agency>
2025-12-24 19:25:47 +0530

    Update RequireRole.tsx

--- RequireRole.tsx
+++ RequireRole.tsx
@@ -1,7 +1,3 @@
-import { useAuth } from "@/context/AuthContext";
-
-export default function RequireRole({ role, children }: any) {
-  const user = useAuth();
-  if (!user || user.role !== role) return null;
+export default function RequireRole({ children }: any) {
   return children;
 }
