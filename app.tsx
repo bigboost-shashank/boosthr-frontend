030c636d8df7669f7220eeb1e25738cdda717a70
bigboost-shashank <shashank@bigboost.agency>
2025-12-24 19:23:59 +0530

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,11 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import AppLayout from "@/components/layout/AppLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
-  );
+  return <Component {...pageProps} />;
 }
